1abc8509d2d1681cc48e1c773e6b434c5e3b8d1c
Melusinee <44269357+Melusinee@users.noreply.github.com>
2019-10-02 23:27:12 +1000

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -86,7 +86,7 @@ class BertConfig(object):
     @classmethod
     def from_json_file(cls, json_file):
         """Constructs a `BertConfig` from a json file of parameters."""
-        with open(json_file, "r") as reader:
+        with open(json_file, "r",encoding = 'utf8') as reader:
             text = reader.read()
         return cls.from_dict(json.loads(text))
 
